28c3f7ac3e062c5019506a9cac50c836bef88618
markor87 <mradovanovic@msn.com>
2022-09-13 13:44:24 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.print("Hello world!\n Marko\n");
-        System.out.println("Hello world!!!\n");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Unesite ime? ");
+        String ime = scanner.nextLine();
+
+        System.out.println("Unesite godinu rodjenja? " );
+        int god = scanner.nextInt();
+        scanner.nextLine(); //koristiti nakon scanner.nextInt();
+
+        System.out.println("Hello " + ime + ", ti imas " + god + " godina.");
     }
 }
