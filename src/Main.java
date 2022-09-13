00fd38c2e9f70e61391942753eecd16d72634962
markor87 <mradovanovic@msn.com>
2022-09-13 11:38:21 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.print("Hello world!\n");
+        System.out.print("Hello world!\n Marko\n");
         System.out.println("Hello world!!!\n");
 
     }
