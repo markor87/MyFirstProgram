a1cf89c35bbbc8f9f77a2e21f4cd86d6b6eca669
markor87 <mradovanovic@msn.com>
2022-09-14 13:32:15 +0200

    commit

--- src/math_class.java
+++ src/math_class.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 public class math_class {
     public static void main(String[] args) {
         double x;
         double y;
         double z;
-        Scanner scanner = new Scanner(System.in);
+        /*Scanner scanner = new Scanner(System.in);
 
         //String ime = scanner.nextLine();
         System.out.println("Unesite duzinu prve stranice.");
@@ -14,7 +15,16 @@ public class math_class {
         y=scanner.nextDouble();
 
         z = Math.sqrt((x*x) + (y*y));
-        System.out.println("Hipotenuza je " + z);
+        System.out.println("Hipotenuza je: " + z + "cm");
+
+        scanner.close();*/
+
+       x = Double.parseDouble(JOptionPane.showInputDialog("Unesi duzinu prve stranice"));
+       y = Double.parseDouble(JOptionPane.showInputDialog("Unesi duzinu druge stranice"));
+       z = Math.sqrt((x*x) + (y*y));
+
+       JOptionPane.showMessageDialog(null, "Duzina hipotenuze je: " + z + "cm");
+
 
     }
 }
