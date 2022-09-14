52709a70cc26154c0cebe35d9afb50147e028035
markor87 <mradovanovic@msn.com>
2022-09-14 12:03:50 +0200

    commit

---
+++ src/gui_intro.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class gui_intro {
+    public static void main(String[] args) {
+
+        String ime = JOptionPane.showInputDialog("Unesi svoje ime");
+        JOptionPane.showMessageDialog(null, "Hello " + ime);
+
+        int godine = Integer.parseInt(JOptionPane.showInputDialog("Koliko imas godina? "));
+
+        JOptionPane.showMessageDialog(null, ime + " " + "ima " + godine + "godina.");
+
+        double visina = Double.parseDouble(JOptionPane.showInputDialog("Koliko si visok? "));
+
+        JOptionPane.showMessageDialog(null, ime + " " + "ima " + godine + "godina. I visok je " + visina + "m");
+
+    }
+}
