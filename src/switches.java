abf5b2efc7edd9fb1bad54ab01542c3b262a3970
markor87 <mradovanovic@msn.com>
2022-09-15 11:28:02 +0200

    commit

--- src/switches.java
+++ src/switches.java
@@ -1,5 +1,22 @@
 public class switches {
     public static void main(String[] args) {
 
+        String dan = "Petak";
+
+        switch (dan){
+            case "Nedelja":
+                System.out.println("Nedelja je!");
+                break;
+            case "Ponedeljak":
+                System.out.println("Ponedeljak je");
+                break;
+            case "Petak":
+                System.out.println("Petak je konacno!");
+                break;
+            default:
+                System.out.println("Nijedan dan!");
+
+        }
+
     }
 }
