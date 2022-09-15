67e2b518f1c6c0aa004d07000775b3578f87f5a2
markor87 <mradovanovic@msn.com>
2022-09-15 14:00:40 +0200

    commit

---
+++ src/while_loop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class while_loop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String ime = "";
+
+        while (ime.isBlank()) {
+            System.out.println("Unesi ime: ");
+            ime = scanner.nextLine();
+        }
+        System.out.println("Hello " + ime);
+    }
+}
