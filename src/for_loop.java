e3859fa571493e94e604683b54439b62b263076a
markor87 <mradovanovic@msn.com>
2022-09-15 14:19:01 +0200

    commit

---
+++ src/for_loop.java
@@ -0,0 +1,9 @@
+public class for_loop {
+    public static void main(String[] args) {
+
+        for (int i=1; i<=17; i+=2) {
+            System.out.println("Imas " + i + " godina");
+        }
+
+    }
+}
