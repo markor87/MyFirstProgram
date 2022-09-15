7e3042c6be5911e02990639421fee523d87d0f5d
markor87 <mradovanovic@msn.com>
2022-09-15 13:18:17 +0200

    commit

---
+++ src/logical_operators.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class logical_operators {
+    public static void main(String[] args) {
+
+        /*&& = i
+        || = ili
+        ! = not*/
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Pritisnite q ili Q da izadjete iz djire.");
+
+        String odgovor = scanner.next();
+
+        if(odgovor.equals("q") || odgovor.equals("Q")) {
+            System.out.println("Quit");
+        }
+        else {
+            System.out.println("I dalje si u djiri");
+        }
+
+    }
+}
