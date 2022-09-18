193c30a92f77892efd5856728715fe441ec802fb
Badji87 <mradovanovic@msn.com>
2022-09-18 11:33:11 +0200

    commit

--- src/for_loop.java
+++ src/for_loop.java
@@ -2,7 +2,7 @@ public class for_loop {
     public static void main(String[] args) {
 
         for (int i=1; i<=17; i+=2) {
-            System.out.println("Imas " + i + " godina");
+            System.out.println("Imas " + i + " godinaa");
         }
 
     }
