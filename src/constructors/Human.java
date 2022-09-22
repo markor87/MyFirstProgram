3c208b81d2c1e285af9adcf130bcb123c3540530
markor87 <mradovanovic@msn.com>
2022-09-22 15:02:50 +0200

    commit

--- src/constructors/Human.java
+++ src/constructors/Human.java
@@ -1,7 +1,20 @@
 package constructors;
 
-public class Human{
-    Human(String ime, int god, double tezina){
+public class Human {
+
+    String ime;
+    int god;
+    double tezina;
+
+    Human(String ime, int god, double tezina) {
+
+        this.ime = ime;
+        this.god = god;
+        this.tezina = tezina;
+
+        }
+    void ishrana(){
+        System.out.println(this.ime+" jede gurmannsku");
 
     }
 }
--- src/constructors/Main.java
+++ src/constructors/Main.java
@@ -2,6 +2,12 @@ package constructors;
 
 public class Main {
     public static void main(String[] args) {
-        Human human = new Human("Marko", 34, 79);
+
+        Human human = new Human("Marko", 34, 79.4);
+        Human human2 = new Human("Rade",72,78);
+
+//        System.out.println(human.ime);
+        System.out.println();
+        human.ishrana();
     }
 }
