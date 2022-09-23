9592504d6d2cd35e3807d92faf6c15dc784471cf
markor87 <mradovanovic@msn.com>
2022-09-23 12:48:50 +0200

    commit

---
+++ src/toString_method/Automobil.java
@@ -0,0 +1,13 @@
+package toString_method;
+
+public class Automobil {
+    String marka = "Kia";
+    String model = "Ceed";
+    int god = 2020;
+    String boja = "bela";
+
+    public String toString() {
+        return marka+" "+ model+" "+god+" "+boja;
+    }
+
+}
---
+++ src/toString_method/Main.java
@@ -0,0 +1,8 @@
+package toString_method;
+
+public class Main {
+    public static void main(String[] args) {
+        Automobil automobil = new Automobil();
+        System.out.println(automobil);
+    }
+}
