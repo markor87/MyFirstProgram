70833dd3f27afd1bf7a2b8dbef5578c0962d8efe
markor87 <mradovanovic@msn.com>
2022-09-27 12:50:36 +0200

    commit

---
+++ src/inheritance/Main.java
@@ -0,0 +1,19 @@
+package inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Bicycle bike = new Bicycle();
+
+        car.go();
+        bike.stop();
+
+        System.out.println(car.speed);
+        System.out.println(bike.speed);
+
+        System.out.println(car.doors);
+        System.out.println(bike.pedals);
+
+    }
+
+}
---
+++ src/inheritance/Vehicle.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class Vehicle {
+    double speed;
+
+    void go(){
+        System.out.println("Vozilo se krece");
+    }
+
+    void stop(){
+        System.out.println("Vozilo je stalo");
+    }
+}
